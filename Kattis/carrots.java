9bb681d2830900e1879eb3f64e0c47c31711e4d1
Tommy Kang <tommykang77@naver.com>
2023-01-15 17:34:59 +0000

    Solve Easy UVa and Kattis problems

--- Kattis/carrots.java
+++ Kattis/carrots.java
@@ -4,6 +4,7 @@ public class carrots {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    sc.nextInt();
     int p = sc.nextInt();
     System.out.println(p);
   }
